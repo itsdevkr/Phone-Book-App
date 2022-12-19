d33d2ae7621c02d3dfc0273f88b91fea14bb214e
itsdevkr <kdev2817@gmail.com>
2022-12-19 16:30:41 +0530

    swagger configured

---
+++ src/main/java/in/ashokit/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package in.ashokit.config;
+
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket apiDoc() {
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+				   .select()
+				   .apis(RequestHandlerSelectors.basePackage("in.ashokit.controller"))
+				   .paths(PathSelectors.any())
+				   .build();
+	}
+}
--- src/main/java/in/ashokit/controller/ContactController.java
+++ src/main/java/in/ashokit/controller/ContactController.java
@@ -3,6 +3,7 @@ package in.ashokit.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,20 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import in.ashokit.binding.Contact;
 import in.ashokit.service.ContactService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
+@CrossOrigin
+@Api(value = "This is contact API")
 public class ContactController {
 	
 	@Autowired
 	private ContactService contactService;
 
 	@PostMapping("/contact")
+	@ApiOperation(value = "This operation is used to create new contact")
 	public String createContact(@RequestBody Contact contact) {
 		String status = contactService.saveContact(contact);
 		return status;
 	}
 	
 	@GetMapping("/contacts")
+	@ApiOperation(value = "This operation is used to get all contacts")
 	public List<Contact> getAllContacts(){
 		List<Contact> allContacts = contactService.getAllContacts();
 		return allContacts;
